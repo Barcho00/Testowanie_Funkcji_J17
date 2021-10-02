8a4457c34b5e33181444d73b8279afe8fac02d68
barcho00 <barcho00@gmail.com>
2021-10-02 16:24:52 +0200

    Initial commit

---
+++ src/pl/func/Main.java
@@ -0,0 +1,19 @@
+package pl.func;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("----------------------------------------------");
+        System.out.println("-------Java 17 Is launched--------------------");
+
+
+        //some code
+
+
+
+        //some more code
+
+
+        System.out.println("-------Shutting down Java 17------------------");
+        }
+}
